5203f06c768d4e4dfb60f7a4adcaa2f11d4d46b4
Oliver Dalgaard <oliver@ihd.dk>
2025-03-30 20:44:37 +0200

    Setup of frontpage
    Co-Authored-By: Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>

---
+++ src/app/(frontpage)/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { FaCartShopping } from "react-icons/fa6";
+import Link from "next/link";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="min-h-screen bg-backdrop">
+      <header className="h-16 w-screen z-50 bg-navbar flex flex-row">
+        <div className="m-auto ml-5 mr-0 flex flex-row">
+          <Link href="/">
+            <FaCartShopping className="m-auto size-8" />
+          </Link>
+          <h1 className="ml-5 text-2xl">Shopping List</h1>
+        </div>
+        <nav className="ml-5 m-auto"></nav>
+      </header>
+      <main className={`pt-16 transition-all duration-300`}>{children}</main>
+    </div>
+  );
+}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 interface props {
   isExpanded: boolean;
 }
@@ -7,9 +8,13 @@ interface props {
 export default function Sidebar(props: props) {
   return (
     <div
-      className={`fixed h-full top-16 left-0 bg-foreground2 z-10 ${
+      className={`fixed h-full top-16 left-0 bg-foreground2 z-10 transition-all duration-250 ${
         props.isExpanded ? "w-64" : "w-0"
       }`}
-    ></div>
+    >
+      <nav className="overflow-hidden">
+        <Link href="">Testing</Link>
+      </nav>
+    </div>
   );
 }
